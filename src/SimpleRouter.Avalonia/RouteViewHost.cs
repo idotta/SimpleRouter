7cd46904986e93dffae7f658b9fe673a2cc2fbf7
iuri dotta <idotta@neurovirtual.com>
2024-06-01 14:14:49 -0300

    Added SimpleRouter.Avalonia with a RouteViewHost and a ViewLocatorBase, to provide functionality for routing views and creating view locators in an Avalonia application.
    RouteViewHost extends the TransitioningContentContro" class and provides functionality for routing views in an Avalonia application.
    ViewLocatorBase implements the "IDataTemplate" interface and provides a base for creating view locators in an Avalonia application without using ReactiveUI's ViewLocator functionalities.

---
+++ src/SimpleRouter.Avalonia/RouteViewHost.cs
@@ -0,0 +1,126 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+
+namespace SimpleRouter.Avalonia;
+
+public class RouteViewHost : TransitioningContentControl
+{
+    /// <summary>
+    /// <see cref="AvaloniaProperty"/> for the <see cref="Router"/> property.
+    /// </summary>
+    public static readonly StyledProperty<IRouter?> RouterProperty =
+        AvaloniaProperty.Register<RouteViewHost, IRouter?>(nameof(Router));
+
+    /// <summary>
+    /// <see cref="AvaloniaProperty"/> for the <see cref="DefaultContent"/> property.
+    /// </summary>
+    public static readonly StyledProperty<object?> DefaultContentProperty =
+        AvaloniaProperty.Register<RouteViewHost, object?>(nameof(DefaultContent));
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RoutedViewHost"/> class.
+    /// </summary>
+    public RouteViewHost()
+    {
+        RouterObserver routerObserver = new(this);
+        RouterProperty.Changed.Subscribe(routerObserver);
+    }
+
+    private sealed class RouterObserver : IObserver<AvaloniaPropertyChangedEventArgs<IRouter?>>
+    {
+        private readonly RouteViewHost _host;
+
+        public RouterObserver(RouteViewHost host)
+        {
+            _host = host ?? throw new ArgumentNullException(nameof(host));
+        }
+
+        public void OnCompleted()
+        {
+        }
+
+        public void OnError(Exception error)
+        {
+        }
+
+        public void OnNext(AvaloniaPropertyChangedEventArgs<IRouter?> value)
+        {
+            var (oldValue, newValue) = value.GetOldAndNewValue<IRouter?>();
+            if (oldValue != null)
+            {
+                oldValue.OnRouteChanged -= NewValue_OnRouteChanged;
+            }
+            if (newValue != null)
+            {
+                newValue.OnRouteChanged += NewValue_OnRouteChanged;
+            }
+        }
+
+        private void NewValue_OnRouteChanged(object? sender, RouteChangedEventArgs e)
+        {
+            _host.NavigateToRoute(e.Next);
+        }
+    }
+
+    /// <summary>
+    /// Gets or sets the <see cref="IRouter"/> of the view model stack.
+    /// </summary>
+    public IRouter? Router
+    {
+        get => GetValue(RouterProperty);
+        set => SetValue(RouterProperty, value);
+    }
+
+    /// <summary>
+    /// Gets or sets the content displayed whenever there is no page currently routed.
+    /// </summary>
+    public object? DefaultContent
+    {
+        get => GetValue(DefaultContentProperty);
+        set => SetValue(DefaultContentProperty, value);
+    }
+
+    /// <summary>
+    /// Gets or sets the view locator used by this router.
+    /// </summary>
+    public ViewLocatorBase? ViewLocator { get; set; }
+
+    protected override Type StyleKeyOverride => typeof(TransitioningContentControl);
+
+    /// <summary>
+    /// Invoked when <see cref="Router"/> navigates to a new route.
+    /// </summary>
+    /// <param name="route">Route to which the user navigates.</param>
+    private void NavigateToRoute(IRoute? route)
+    {
+        if (Router == null)
+        {
+            System.Diagnostics.Debug.WriteLine("Router property is null. Falling back to default content.");
+            Content = DefaultContent;
+            return;
+        }
+
+        if (route == null)
+        {
+            System.Diagnostics.Debug.WriteLine("Route is null. Falling back to default content.");
+            Content = DefaultContent;
+            return;
+        }
+
+        if (ViewLocator == null)
+        {
+            Content = route;
+            return;
+        }
+
+        var view = ViewLocator.Build(route);
+        if (view == null)
+        {
+            Content = DefaultContent;
+            return;
+        }
+        
+        view.DataContext = route;
+        Content = view;
+    }
+}
---
+++ src/SimpleRouter.Avalonia/ViewLocatorBase.cs
@@ -0,0 +1,77 @@
+﻿using Avalonia.Controls;
+using Avalonia.Controls.Templates;
+
+namespace SimpleRouter.Avalonia;
+
+/// <summary>
+/// Base class for view locators that implement the IDataTemplate interface.
+/// </summary>
+public abstract class ViewLocatorBase : IDataTemplate
+{
+    /// <summary>
+    /// Gets or sets the default content to be used when no specific control is resolved.
+    /// </summary>
+    public Control? DefaultContent { get; set; }
+
+    /// <summary>
+    /// Builds the control based on the provided parameter.
+    /// If the parameter is null or can't be resolved, the default content is returned.
+    /// </summary>
+    /// <param name="param">The parameter used to build the control.</param>
+    /// <returns>The built control.</returns>
+    public virtual Control? Build(object? param)
+    {
+        if (param == null)
+        {
+            return DefaultContent;
+        }
+        var control = ResolveControl(param as IRoute);
+        if (control != null)
+        {
+            return control;
+        }
+        control = TryDeduceControl(param);
+        if (control != null)
+        {
+            return control;
+        }
+        return DefaultContent;
+    }
+
+    /// <summary>
+    /// Determines if the provided data matches the route.
+    /// You can override this method to provide custom matching logic.
+    /// </summary>
+    /// <param name="data">The data to be matched.</param>
+    /// <returns>True if the data matches the route; otherwise, false.</returns>
+    public virtual bool Match(object? data)
+    {
+        return data is IRoute;
+    }
+
+    /// <summary>
+    /// Resolves the control based on the provided route.
+    /// It is highly recommended that you implement this method by matching each view to its
+    /// viewmodel instead of using <see cref="TryDeduceControl"/>.
+    /// If you still don't want to implement it, you can return null directly.
+    /// </summary>
+    /// <param name="route">The route used to resolve the control.</param>
+    /// <returns>The resolved control.</returns>
+    protected abstract Control? ResolveControl(IRoute? route);
+
+    /// <summary>
+    /// Tries to deduce the control based on the provided data by using reflection.
+    /// </summary>
+    /// <param name="data">The data used to deduce the control.</param>
+    /// <returns>The deduced control.</returns>
+    private static Control? TryDeduceControl(object data)
+    {
+        var name = data.GetType().FullName?.Replace("ViewModel", "View") ?? "";
+        var type = Type.GetType(name);
+        if (type != null)
+        {
+            return Activator.CreateInstance(type) as Control;
+        }
+        return null;
+    }
+}
